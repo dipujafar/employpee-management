771bc1326ef749eb78bab836d0b0650d8de5e097
dipujafar <dipujafarmr@gmail.com>
2023-11-25 23:05:35 +0600

    start project and set up basic need

---
+++ src/layout/Root.jsx
@@ -0,0 +1,15 @@
+import { Button } from "@material-tailwind/react";
+ 
+// export default function Example() {
+//   return <Button>Button</Button>;
+// }
+
+const Root = () => {
+    return (
+        <div>
+           <Button>Button</Button> 
+        </div>
+    );
+};
+
+export default Root;
