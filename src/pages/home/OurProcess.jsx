b67983bc5785c1cbcea1cfb1ceb2db470686391e
dipujafar <dipujafarmr@gmail.com>
2023-11-27 21:48:43 +0600

    added proces section in home page

--- src/pages/home/OurProcess.jsx
+++ src/pages/home/OurProcess.jsx
@@ -1,9 +1,12 @@
-
+import processImg from '../../assets/image/develop-process.jpg'
 
 const OurProcess = () => {
     return (
         <div className="mt-10">
-            <h1 className="text-3xl text-center uppercase text-orange-400 font-medium">Our Development Process</h1>
+            <h1 className="text-3xl text-center uppercase text-orange-400 font-medium mb-2">Our Development Process</h1>
+            <div className="flex justify-center">
+            <img src={processImg} alt="" className='max-h-[500px]' />
+            </div>
         </div>
     );
 };
